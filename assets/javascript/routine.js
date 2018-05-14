459b71fb732cb972ee59c1fd6b3bd341b22c9cf8
Mark Iudice <markiudice@Marks-MacBook-Pro.local>
2018-05-14 14:10:46 -0400

    adding project to improve

---
+++ assets/javascript/routine.js
@@ -0,0 +1,7 @@
+var routine = [];
+
+
+
+$("#qIcon").on("click", function() {
+    alert("Add workouts to this page to create single routines.  Routines can be saved for later or cleared once the workout is complete");
+})
