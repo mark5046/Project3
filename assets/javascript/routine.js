36d2065f94f8ded9f2b018fdf0b7d8b304639228
Armando Vargas <Mando@Armandos-MacBook-Pro.local>
2018-05-16 12:38:49 -0400

    react foundation

--- assets/javascript/routine.js
+++
@@ -1,7 +0,0 @@
-var routine = [];
-
-
-
-$("#qIcon").on("click", function() {
-    alert("Add workouts to this page to create single routines.  Routines can be saved for later or cleared once the workout is complete");
-})
