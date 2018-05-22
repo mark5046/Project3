8de9570ce3104d133431f8d33e8096ebd069548c
Chris Bacani <chrisbacani@Chriss-MacBook.local>
2018-05-22 13:21:37 -0400

    Added scrape utils and component files

--- fitness-hand/src/App.js
+++ fitness-hand/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+import React from 'react';
+// import './App.css';
+import Nav from './components/nav'
+import Cover from './components/cover.js'
+import Scrape from './components/scrape'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+<div>
+  <Nav />
+
+<Scrape />
+
+</div>
+)
 
 export default App;
---
+++ fitness-hand/src/components/scrape.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import scrape from '../utils/scrape';
+import './scrape.css'
+
+class Scrape extends Component {
+    state = {
+        articles: [],
+    }
+
+    componentDidMount() {
+        scrape().then(articles => {
+            this.setState({ articles });
+        });
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div className="scrapeWrapper">
+            <h3 className>Here are the latest headlines from Men's Journal</h3>
+            <ul className ="scrapeContainer">
+                {this.state.articles.map(article => {
+                    return <li className="row">
+                        <div class="xs-col-2" />
+                        <img src={article.img} />
+
+                        <div class="xs-col-10" />
+                        <a href={article.link} target="_blank"><h5>
+                            {article.title}</h5>
+                        </a>
+                    </li>
+                })}
+            </ul>
+        </div>
+        );
+    }
+}
+
+export default Scrape;
